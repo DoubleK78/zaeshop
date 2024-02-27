2b091657a97d24d149bbbb812fb09b7845d5799f
kietother <161268093+kietother@users.noreply.github.com>
2024-02-27 10:41:36 +0000

    260: Add Migration

---
+++ src/Services/Portal/Portal.Infrastructure/Migrations/20240227103814_OptimizeStorage260.cs
@@ -0,0 +1,227 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptimizeStorage260 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContentItem_CollectionId",
+                table: "ContentItem");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "UserLevel",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "UserLevel",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpV6Address",
+                table: "UserActivityLog",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpV4Address",
+                table: "UserActivityLog",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RelativeUrl",
+                table: "ContentItem",
+                type: "varchar(550)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "ContentItem",
+                type: "varchar(550)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContentItem",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayUrl",
+                table: "ContentItem",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "CollectionView",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "CollectionView",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StorageType",
+                table: "Collection",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentItem_CollectionId",
+                table: "ContentItem",
+                column: "CollectionId")
+                .Annotation("SqlServer:Include", new[] { "RelativeUrl" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContentItem_CollectionId",
+                table: "ContentItem");
+
+            migrationBuilder.DropColumn(
+                name: "StorageType",
+                table: "Collection");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "UserLevel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "UserLevel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpV6Address",
+                table: "UserActivityLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpV4Address",
+                table: "UserActivityLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RelativeUrl",
+                table: "ContentItem",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(550)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "ContentItem",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(550)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContentItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayUrl",
+                table: "ContentItem",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "CollectionView",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "CollectionView",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContentItem_CollectionId",
+                table: "ContentItem",
+                column: "CollectionId")
+                .Annotation("SqlServer:Include", new[] { "DisplayUrl" });
+        }
+    }
+}
