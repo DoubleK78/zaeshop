fe8610a84d37fa81aae11febb4bcd1baacb6bc99
kietother <161268093+kietother@users.noreply.github.com>
2024-03-01 13:33:25 +0000

    267: Update url

--- src/UI/portal/src/services/baseUrls.ts
+++ src/UI/portal/src/services/baseUrls.ts
@@ -1,2 +1,2 @@
 export const identityServer = 'https://codegota.me/identity';
-export const portalServer = 'https://codegota.me/portal';
+export const portalServer = 'https://api-premium.fastscans.net';
