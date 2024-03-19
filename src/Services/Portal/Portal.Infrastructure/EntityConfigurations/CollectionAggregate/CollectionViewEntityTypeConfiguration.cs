8d823d893efed8006fed9832ae082cb5aa804587
Kiet Tran <44864496+kiettran99@users.noreply.github.com>
2024-03-19 14:35:37 +0000

    Update Entity

--- src/Services/Portal/Portal.Infrastructure/EntityConfigurations/CollectionAggregate/CollectionViewEntityTypeConfiguration.cs
+++ src/Services/Portal/Portal.Infrastructure/EntityConfigurations/CollectionAggregate/CollectionViewEntityTypeConfiguration.cs
@@ -12,6 +12,13 @@ namespace Portal.Infrastructure.EntityConfigurations.CollectionAggregate
 
             builder.HasOne(x => x.Collection).WithMany(y => y.CollectionViews).HasForeignKey(z => z.CollectionId);
             builder.HasOne(x => x.User).WithMany(y => y.CollectionViews).HasForeignKey(z => z.UserId);
+
+            // Index To Calculate Views more efficient
+            builder.HasIndex(cv => cv.CollectionId)
+                .IncludeProperties(cv => new { cv.Date, cv.View })
+                .HasDatabaseName("nci_msft_1_CollectionView_394301034398E39CFFB2DD574D69974B")
+                .HasFilter(null)
+                .IsUnique(false);
         }
     }
 }
