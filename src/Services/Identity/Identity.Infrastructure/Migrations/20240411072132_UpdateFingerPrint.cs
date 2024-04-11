c2bca72fe8ae1c1539b5f758e4ab920f1560413b
Kiet Tran <44864496+kiettran99@users.noreply.github.com>
2024-04-11 07:37:49 +0000

    Add-Migration

---
+++ src/Services/Identity/Identity.Infrastructure/Migrations/20240411072132_UpdateFingerPrint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFingerPrint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFingerPrints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FingerPrint = table.Column<string>(type: "varchar(150)", nullable: false),
+                    AdditionalDetail = table.Column<string>(type: "varchar(500)", nullable: true),
+                    IsBanned = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFingerPrints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserFingerPrints_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFingerPrints_UserId",
+                table: "UserFingerPrints",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFingerPrints");
+        }
+    }
+}
