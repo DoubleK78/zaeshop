e57ee71c03f49422788e48c734f65bc66cd79f77
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-10-21 03:38:36 +0000

    313: Increase Time

--- src/Services/Portal/Portal.Infrastructure/Implements/Business/Services/AlbumService.cs
+++ src/Services/Portal/Portal.Infrastructure/Implements/Business/Services/AlbumService.cs
@@ -493,14 +493,14 @@ namespace Portal.Infrastructure.Implements.Business.Services
                     albumFriendlyNames.Add(album.FriendlyName);
                 }
 
-                if (album.LevelPublic == ELevelPublic.SPremiumUser && difference.TotalHours >= 4 && difference.TotalHours < 15)
+                if (album.LevelPublic == ELevelPublic.SPremiumUser && difference.TotalHours >= 4 && difference.TotalHours < 16)
                 {
                     album.LevelPublic = ELevelPublic.PremiumUser;
                     albumFriendlyNames.Add(album.FriendlyName);
                 }
 
                 if ((album.LevelPublic == ELevelPublic.SPremiumUser || album.LevelPublic == ELevelPublic.PremiumUser) &&
-                     difference.TotalHours >= 15)
+                     difference.TotalHours >= 16)
                 {
                     album.LevelPublic = ELevelPublic.AllUser;
                     albumFriendlyNames.Add(album.FriendlyName);
