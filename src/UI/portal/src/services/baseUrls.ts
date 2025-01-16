494530b2653effe73566029a930bf0eb57ac496b
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2025-01-16 01:47:00 +0000

    Update

--- src/UI/portal/src/services/baseUrls.ts
+++ src/UI/portal/src/services/baseUrls.ts
@@ -1,2 +1,2 @@
-export const identityServer = 'https://api.fastscans6.net/identity';
-export const portalServer = 'https://api-premium.fastscans6.net';
+export const identityServer = 'https://api.fastscans7.net/identity';
+export const portalServer = 'https://api-premium.fastscans7.net';
